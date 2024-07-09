e92cbcfb85233b5a2ed6c4e3fa5613d4bc317095
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-07-09 18:51:05 +0200

    Add routes for category & type

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,12 @@ use App\Http\Controllers\Api\ItemController;
 use App\Http\Controllers\Api\StockInController;
 use App\Http\Controllers\Api\StockOutController;
 use App\Http\Controllers\Api\SupplierController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TypeController;
 use Illuminate\Support\Facades\Route;
 
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('types', TypeController::class);
 Route::apiResource('items', ItemController::class);
 Route::apiResource('suppliers', SupplierController::class);
 Route::apiResource('products', StockInController::class);
