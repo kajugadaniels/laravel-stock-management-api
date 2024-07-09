b6c231923bae27d6a112736ec7538af0d00dcbaa
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-07-09 18:47:13 +0200

    Add fields in types migrations

--- database/migrations/2024_07_09_164359_create_types_table.php
+++ database/migrations/2024_07_09_164359_create_types_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->string('name');
             $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
 
