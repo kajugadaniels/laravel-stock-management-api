e9affd0419f989ff1c51c848de6229bc03e28b9a
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-07-09 18:50:31 +0200

    CRUD operation for type

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -7,59 +7,51 @@ use Illuminate\Http\Request;
 
 class TypeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        return response()->json(Type::with('category')->get(), 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'category_id' => 'required|exists:categories,id'
+        ]);
+        $type = Type::create($request->all());
+        return response()->json($type, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Type $type)
+    public function show($id)
     {
-        //
+        $type = Type::with('category')->find($id);
+        if ($type) {
+            return response()->json($type, 200);
+        }
+        return response()->json(['message' => 'Type not found'], 404);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Type $type)
+    public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'name' => 'sometimes|required|string|max:255',
+            'category_id' => 'sometimes|required|exists:categories,id'
+        ]);
+        $type = Type::find($id);
+        if ($type) {
+            $type->update($request->all());
+            return response()->json($type, 200);
+        }
+        return response()->json(['message' => 'Type not found'], 404);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Type $type)
+    public function destroy($id)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Type $type)
-    {
-        //
+        $type = Type::find($id);
+        if ($type) {
+            $type->delete();
+            return response()->json(['message' => 'Type deleted'], 200);
+        }
+        return response()->json(['message' => 'Type not found'], 404);
     }
 }
