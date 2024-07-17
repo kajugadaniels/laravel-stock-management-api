91c4dee881192fedfb2c2a88424e9ed360e77b55
lindabriella <lindabriellas@gmail.com>
2024-07-17 16:14:15 +0200

    emp

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -7,15 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    protected $guarded = [];
+    use HasFactory;
+    protected $fillable = ['name', 'contact', 'position'];
 
-    public function prodStockIns()
-    {
-        return $this->hasMany(StockIn::class);
-    }
-
-    public function prodStockOuts()
-    {
-        return $this->hasMany(StockOut::class);
-    }
 }
