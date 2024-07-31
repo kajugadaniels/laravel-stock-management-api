2362e9cd71b67ed623db62643a95cfe809279a75
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-07-31 16:49:07 +0200

    Create relationship in package request models

--- app/Models/PackageRequest.php
+++ app/Models/PackageRequest.php
@@ -5,7 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PackageRequest extends Model
-{
+class PackageRequest extends Model {
     use HasFactory;
+
+    protected $fillable = [
+        'contact_person_id',
+        'requester_name',
+        'request_from',
+        'status',
+        'request_for_id',
+        'quantity',
+        'note'
+    ];
+
+    public function items() {
+        return $this->hasMany(PackageRequestItem::class);
+    }
+
+    public function contactPerson() {
+        return $this->belongsTo(Employee::class, 'contact_person_id');
+    }
+
+    public function requestFor() {
+        return $this->belongsTo(Item::class, 'request_for_id');
+    }
 }
--- app/Models/PackageRequestItem.php
+++ app/Models/PackageRequestItem.php
@@ -5,7 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PackageRequestItem extends Model
-{
+class PackageRequestItem extends Model {
     use HasFactory;
+
+    protected $fillable = [
+        'package_request_id',
+        'stock_in_id',
+        'quantity'
+    ];
+
+    public function packageRequest() {
+        return $this->belongsTo(PackageRequest::class);
+    }
+
+    public function stockIn() {
+        return $this->belongsTo(StockIn::class);
+    }
 }
