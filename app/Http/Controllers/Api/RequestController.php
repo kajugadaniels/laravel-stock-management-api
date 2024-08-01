0bb7ca32bdc8865332fd9e0e64251d1bb200f107
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-08-01 11:45:13 +0200

    Modify RequestController show function to return detailed data

--- app/Http/Controllers/Api/RequestController.php
+++ app/Http/Controllers/Api/RequestController.php
@@ -92,8 +92,16 @@ class RequestController extends Controller
     {
         try {
             $requestModel = RequestModel::with([
-                'items.item',
-                'items.supplier',
+                'items' => function ($query) {
+                    $query->with([
+                        'item.type',
+                        'item.category',
+                        'item' => function ($itemQuery) {
+                            $itemQuery->select('id', 'name', 'capacity', 'unit');
+                        },
+                        'supplier'
+                    ]);
+                },
                 'contactPerson',
                 'requestFor'
             ])->find($id);
