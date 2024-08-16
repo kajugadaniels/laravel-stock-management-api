9113ba1e6e47598521429ac8357ac6ad6b6a3f74
Admin-dynasoft <dynasoftrw@gmail.com>
2024-08-16 15:49:49 +0200

    Update RegisterController.php

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
-use App\Http\Controllers\API\BaseController as BaseController;
+use App\Http\Controllers\Api\BaseController as BaseController;
 
 class RegisterController extends BaseController
 {
