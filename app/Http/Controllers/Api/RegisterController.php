768ef68d83b9eaa44e5528b2fbdd4dc7e4576b6b
Admin-dynasoft <dynasoftrw@gmail.com>
2024-08-19 14:52:43 +0200

    Update RegisterController.php API to Api

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
-use App\Http\Controllers\API\BaseController as BaseController;
+use App\Http\Controllers\Api\BaseController as BaseController;
 
 class RegisterController extends BaseController
 {
